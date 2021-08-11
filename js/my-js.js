db9b090726b1e5e5f21260bd386ef353e26e2412
reham90 <rehamadel344@gmail.com>
2021-08-11 12:35:03 +0300

    edit

--- js/my-js.js
+++ js/my-js.js
@@ -1,94 +1,4 @@
 $(document).ready(function() {
-    if($(window).width()<991){
-
- (function($) { // Begin jQuery36.
-
-  $(function() { // DOM ready
-    // If a link has a dropdown, add sub menu toggle.
-    $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').slideToggle();
-      // Close one dropdown when selecting another
-      
-      e.stopPropagation();
-    });
-
-     
-
-    // Clicking away from dropdown will remove the dropdown class
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
-    });
-
-   $('.dropdown').click(function(e) {
-      $('.drop-mobile').slideToggle();
-      // Close one dropdown when selecting another
-      
-      e.stopPropagation();
-    });
-
-    // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function() {
-    
-     
-
-$("#re-show").toggle();
-
-$("#re-hide").css("display" ,"block");
-
-$("#cover").toggle();
-
-
-    });
-
-  $('#re-show').click(function(event) {
-
- if (document.dir=="rtl") {
-
-     $('nav ').css("right" , "0");
-}
-else{
-    $('nav ').css("left" , "0");
-
-}
-    $("body").css("overflow" , "hidden");
-
-
-$("#nav").toggle();
-  
-
-  if ($('.nav-list').hasClass('dismiss')) {
-    $('.nav-list').removeClass('dismiss').addClass('selected').show();
-  }
-  event.preventDefault();
-});
-
-$('#re-hide').click(function(event) {
-  if ($('.nav-list').hasClass('selected')) {
-    $('.nav-list').removeClass('selected').addClass('dismiss');
-  }
-  event.preventDefault();
- $("body").css("overflow" , "auto");
-
- $("#cover").toggle();
-
- $("#re-show").toggle();
-});
-
- $('#cover').on('click', function() {
-$("#re-hide").click();
- });
-
-
-    // Hamburger to X toggle
-    
-  }); // end DOM ready
-})(jQuery); // end jQuery83"9?"
-
-
-    }
-});
-
-$(document).ready(function() {
     if($(window).width()>991){
 
 (function($) { // Begin jQuery36.
@@ -178,7 +88,95 @@ $("#re-hide").click();
 }
 });
 
+$(document).ready(function() {
+    if($(window).width()<991){
 
+ (function($) { // Begin jQuery36.
+
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').slideToggle();
+      // Close one dropdown when selecting another
+      
+      e.stopPropagation();
+    });
+
+     
+
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+
+   $('.dropdown').click(function(e) {
+      $('.drop-mobile').slideToggle();
+      // Close one dropdown when selecting another
+      
+      e.stopPropagation();
+    });
+
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+    
+     
+
+$("#re-show").toggle();
+
+$("#re-hide").css("display" ,"block");
+
+$("#cover").toggle();
+
+
+    });
+
+  $('#re-show').click(function(event) {
+
+ if (document.dir=="rtl") {
+
+     $('nav ').css("right" , "0");
+}
+else{
+    $('nav ').css("left" , "0");
+
+}
+    $("body").css("overflow" , "hidden");
+
+
+$("#nav").toggle();
+  
+
+  if ($('.nav-list').hasClass('dismiss')) {
+    $('.nav-list').removeClass('dismiss').addClass('selected').show();
+  }
+  event.preventDefault();
+});
+
+$('#re-hide').click(function(event) {
+  if ($('.nav-list').hasClass('selected')) {
+    $('.nav-list').removeClass('selected').addClass('dismiss');
+  }
+  event.preventDefault();
+ $("body").css("overflow" , "auto");
+
+ $("#cover").toggle();
+
+ $("#re-show").toggle();
+});
+
+ $('#cover').on('click', function() {
+$("#re-hide").click();
+ });
+
+
+    // Hamburger to X toggle
+    
+  }); // end DOM ready
+})(jQuery); // end jQuery83"9?"
+
+
+    }
+});
 
 
 
